c7688ce3dda8bee4f231cf1728c5bd7fd23374a7
feherke <feherke@master>
2012-03-08 14:19:57 +0200

    changed highlighting

--- tipmaster-author-highlight/TipMaster_-_Author_Highlight.user.js
+++ tipmaster-author-highlight/TipMaster_-_Author_Highlight.user.js
@@ -4,12 +4,28 @@
 // @description    Highlights the OP's and own messages in a thread.
 // @match          http://*.tek-tips.com/viewthread.cfm?qid=*
 // @match          http://*.eng-tips.com/viewthread.cfm?qid=*
-// @version        0.0
+// @version        0.1
 // ==/UserScript==
 
 
-GM_addStyle('table.gm_own { background-image: -moz-linear-gradient(left,#930 3px,rgba(153,51,0,0) 10px); }')
-GM_addStyle('table.gm_op { box-shadow: #930 -10px 0 3px -3px; }')
+GM_addStyle('table.gm_own { background-image: -moz-linear-gradient(left,rgba(153,51,0,.5),rgba(153,51,0,0) 15px), -moz-element(#gm_ownbg); background-repeat: no-repeat; }')
+GM_addStyle('table.gm_op { background-image: -moz-linear-gradient(right,rgba(153,51,0,.5),rgba(153,51,0,0) 15px), -moz-element(#gm_opbg); background-repeat: no-repeat; background-position: right top; }')
+
+GM_addStyle('div.gm_bg { position: absolute; top: -1000px; display: inline; color: #930; font-size: 150pt; opacity: .15; }')
+GM_addStyle('div#gm_ownbg { -moz-transform: rotate(-10deg); }')
+GM_addStyle('div#gm_opbg { -moz-transform: rotate(10deg); }')
+
+GM_addStyle('.tgml blockquote { border-color: rgba(205,205,255,.66); !important; }')
+GM_addStyle('blockquote { background-color: rgba(238,238,255,.75) !important; }')
+GM_addStyle('.tgml blockquote h4 { background-color: transparent !important; }')
+
+GM_addStyle('div#code { border-color: rgba(0,0,0,.66); }')
+GM_addStyle('div#code .body { background-color: rgba(255,255,255,.75) !important; }')
+GM_addStyle('.tgml div#code h4 { background-color: rgba(119,119,119,.75) !important; }')
+
+GM_addStyle('div.spoiler { background-color: transparent !important; border-color: rgba(0,0,0,.66); }')
+GM_addStyle('div.spoiler .body { color: transparent !important; background-color: rgba(255,255,255,.75) !important; }')
+GM_addStyle('.tgml div.spoiler h4 { background-color: rgba(255,0,0,.75) !important; }')
 
 
 var ownhandle=document.querySelector('div.tagline+div:not(.wrapper)')
@@ -25,3 +41,12 @@ for (var i=0,post;post=list[i++];) {
   if (handle==ownhandle) post.classList.add('gm_own')
   if (handle==ophandle) post.classList.add('gm_op')
 }
+
+
+['own','OP'].forEach(function (one) {
+  var div=document.createElement('div')
+  div.innerHTML=one
+  div.id='gm_'+one.toLowerCase()+'bg'
+  div.className='gm_bg'
+  document.body.appendChild(div)
+})
